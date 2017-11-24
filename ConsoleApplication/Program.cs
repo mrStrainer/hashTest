ada5b877df223ec265dd3c8d91f9cfd2c0fb34a5
Attila Szita <atee88@gmail.com>
2017-11-24 15:53:21 +0100

    Initial commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("User:");
+            var user = Console.ReadLine();
+            
+            Console.WriteLine("Pass:");
+            var pass = Console.ReadLine();
+            Console.ReadLine();
+            var hashedPass = SecurePasswordHasher.Hash(pass);
+            var isEqual = SecurePasswordHasher.Verify(pass, hashedPass);
+            
+            Console.WriteLine($"User: { user }\n Pass: { pass }");
+            Console.WriteLine($"Hashed pass: { hashedPass } \n Verification: { isEqual }");
+        }
+    }
+}
